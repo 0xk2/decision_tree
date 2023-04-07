0b5a5f0afa177db259d2979d615fc2bb25fd6a7c
Hieu <trunghieubui88@gmail.com>
2023-04-07 11:22:52 +0700

    add port

--- decision_tree/network/network.go
+++ decision_tree/network/network.go
@@ -3,17 +3,30 @@ package network
 import (
 	"log"
 	"net/http"
+	"os"
 
 	. "github.com/0xk2/decision_tree/decision_tree/dtree"
+	"github.com/joho/godotenv"
 )
 
 var Missions = make(map[string]*Mission)
 
 func GoServeTree() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	port := os.Getenv("PORT")
 	http.HandleFunc("/create", CreateHandler)
 	http.HandleFunc("/vote", VoteHandler)
 	http.HandleFunc("/show", ShowHandler)
-	log.Println("Server start at 8080")
+	http.HandleFunc("/check", healthCheck)
+	log.Println("Server start at " + port)
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":"+port, nil)
+}
+
+func healthCheck(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/0xk2/decision_tree
 
 go 1.20
+
+require github.com/joho/godotenv v1.5.1 // indirect
